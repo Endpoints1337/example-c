00c5c807415e120cfc21a7b1d60fddf4f8e656ed
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-03-06 08:56:10 -0600

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,19 +1,20 @@
-#Include <iostream>
-#Include <windows.h>
+#Include <iostream> // basic input output for c++
+#Include <windows.h> // use this if you are on windows just helps with beginer stuff
 
 
+  static bool func = false;
 
-usign namespace std;
-
-
-
-
-void example() {
-  
+void example(func) 
+{    
+  if (funct)
+  {
+    cout << "Hello World!"; // this will just print hello world!
+    printf("Hello World!"); // same thing but for only windows.
+  }
 }
 
 // This is the main function so this is where your code will beging you can make varaibles to change this tho
-int main() {
-  
-  example(); // by making a void above the function you will begin to hav
+int main() 
+{  
+  example(); // by making a void above the function you will begin to have a varaible.
 }

4ae63245229d60474a8e275a91ed25435b13217f
Endpoints1337 <115175031+Endpoints1337@users.noreply.github.com>
2023-03-06 08:51:31 -0600

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,19 @@
+#Include <iostream>
+#Include <windows.h>
+
+
+
+usign namespace std;
+
+
+
+
+void example() {
+  
+}
+
+// This is the main function so this is where your code will beging you can make varaibles to change this tho
+int main() {
+  
+  example(); // by making a void above the function you will begin to hav
+}
